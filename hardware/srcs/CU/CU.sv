cc663c5f3577919200daa92726746a30c945f51c
WachirawichN <wnamgird@gmail.com>
2026-06-20 21:33:09 +0700

    Added file for Control Unit.

---
+++ hardware/srcs/CU/CU.sv
@@ -0,0 +1,7 @@
+`timescale 1ns/1ps
+
+module CU (
+        input logic clk,
+        input logic [2:0] func_3,
+    );
+endmodule
---
+++ hardware/srcs/CU/OpcodeDecoder.sv
@@ -0,0 +1,6 @@
+`timescale 1ns/1ps
+
+module OpcodeDecoder (
+        input logic [6:0] opcode,
+    );
+endmodule
